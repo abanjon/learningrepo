7c736d4401c77e61808802c829e783bd15fa3f91
abanjon <aban.jones@getcrystal.com>
2026-02-15 17:00:41 -0500

    creating schemas

---
+++ soundstream/db.py
@@ -0,0 +1,12 @@
+# db.py
+import psycopg2
+
+
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        port=5432,
+        dbname="soundstream_dev",
+        user="admin",
+        password="admin",
+    )
